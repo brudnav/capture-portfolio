16a7e72bbebcb86ab67149e81117eda607f88600
vaclav <brudna.vaclav@seznam.cz>
2022-10-02 13:08:28 +0200

    9. React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContantUs from "./pages/ContantUs";
+
 import GlobalStyle from "./components/GlobalStyle";
+import {Route, Routes} from "react-router-dom"
 import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+
+      <Routes>
+      <Route path="/" element={<AboutUs/>}>
+      </Route>
+      <Route path="/work" element={<OurWork/>}>
+      </Route>
+      <Route path="/contact" element={<ContantUs/>}>
+      </Route>
+      </Routes>
+       
     </div>
   );
 }
---
+++ src/pages/ContantUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContantUs = () => {
+    return(
+        <h1>Contact Us</h1>
+    )
+}
+
+export default ContantUs;
